aab7a5f0fdc81eaad802ca5b8cd974d3eb18514a
Leandro Pires de Souza <leandro.pires.souza@gmail.com>
2015-02-02 02:52:24 -0500

    Adding effect to the contact buttons and more
    Also improving the projects modal-window

--- resume/www/js/app.js
+++ resume/www/js/app.js
@@ -20,13 +20,44 @@ angular.module('starter', ['ionic'])
   });
 })
 */
+var chat = { pics: [{src:""},{src:""}], title:"chat", idea: "Here it goes...", builtWith: [{img:"", href:""}]
+};
 
+function fillProject(json){
+	$("div.project div.body h1.project-title").text(json.title);
+	$("div.project div.body p.description").text(json.idea);
+	$("div.project div.body div.built-with").empty();
+	jQuery.each(json.builtWith, function(i, src){
+		$("div.project div.body div.built-with").append("<img src='"+src.img+"' href='"+src.href+"'>");
+	});
+}
 
 $(function(){
+	
+	$(window).scroll(function() {
+	   if($(window).scrollTop() + $(window).height() > $(document).height() - 10) {
+		   $("div.section-contact button").addClass("up");
+	   }
+	});
+	
 	$("img.item-carousel").click(function(){
 		var pic = $(this).css("background-image");
 		$("div.img-big").css("background-image", pic);
 	});
 	
-	$("section-projects div.project-list div.project-item").click(function(){
+	$("div.section-projects div.projects-list div.project-item ").click(function(){
+		fillProject(chat);
+		$(this).addClass("expanded");
+		$("div.cover").addClass("expanded");
+		$("div.whole").addClass("blur");
+		$("div.project").addClass("expanded");
+		
+	});
+	
+	$("div.cover").click(function(){
+		$(this).removeClass("expanded");
+		$("div.cover").removeClass("expanded");
+		$("div.whole").removeClass("blur");
+		$("div.project").removeClass("expanded");
+	});
 });
