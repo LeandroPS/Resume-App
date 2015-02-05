c7b1ed4b91d36377ec17916d8244157162a5a6c4
Leandro Pires de Souza <leandro.pires.souza@gmail.com>
2015-02-05 02:27:02 -0500

    reducing images with gulp

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var imageResize = require('gulp-image-resize');
+ 
+gulp.task('contact-button', function () {
+  gulp.src('img/contact-buttons/*.png')
+    .pipe(imageResize({ 
+      width : 40,
+      height : 40,
+      crop : false,
+      upscale : false
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('photos', function () {
+  gulp.src('img/photos/*.{jpg,png}')
+    .pipe(imageResize({ 
+      width : 300,
+      height : 300,
+      crop : false,
+      upscale : false
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('default', function () {
+  	gulp.run('contact-button');
+	gulp.run('photos');
+
+});
+
