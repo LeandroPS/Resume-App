f4b9369560afef464533fbcc763f6b60a44c555f
Leandro Pires de Souza <leandro.pires.souza@gmail.com>
2015-02-07 01:52:01 -0500

    final modifications

--- js/app.js
+++ js/app.js
@@ -20,13 +20,13 @@ angular.module('starter', ['ionic'])
   });
 })
 */
-var chat = { pics: "img/proj-prints/chat1.png", title:"chat", idea: "A chat to be used in a video broadcast website being developed by a friend, The chat allows multiple rooms, status, and users with different privileges", builtWith: [{img:"img/Proj-logos/HTML5_LOGO.svg", href:""},{img:"img/Proj-logos/socket io logo.svg", href:"http://http://socket.io"},{img:"img/Proj-logos/nodejs-logo.png", href:""}], gitHub: false
+var chat = { pics: "img/proj-prints/chat1.png", title:"chat", idea: "A chat system to be used on a video broadcast website that is being developed by a friend. The chat system allows multiple rooms, statuses, and users with different privileges.", builtWith: [{img:"img/Proj-logos/HTML5_LOGO.svg", href:""},{img:"img/Proj-logos/socket io logo.svg", href:"http://http://socket.io"},{img:"img/Proj-logos/nodejs-logo.png", href:""}], gitHub: false
 };
 
-var tsk = { pics: "img/proj-prints/tsk.png", title:"Tsk", idea: "A Task Manager with a simplified experience and that uses your Google or Dropbox account to store your content and keep it synchronized between multiple devices and a Chrome Extension", builtWith: [{img:"img/Proj-logos/HTML5_LOGO.svg", href:""},{img:"img/Proj-logos/ionic_logo.png", href:"http://http://socket.io"},{img:"img/Proj-logos/jQuery-Logo.png", href:""}], gitHub: false
+var tsk = { pics: "img/proj-prints/tsk.png", title:"Tsk", idea: "A To-Do-List Manager offering a simplified experience and giving the user the choice to use their Google or Dropbox account to store their content and keep it synchronized between multiple devices. Implemented alongside a Google Chrome Extension.", builtWith: [{img:"img/Proj-logos/HTML5_LOGO.svg", href:""},{img:"img/Proj-logos/ionic_logo.png", href:"http://http://socket.io"},{img:"img/Proj-logos/jQuery-Logo.png", href:""}], gitHub: false
 };
 
-var slides = { pics: "img/proj-prints/slides.png", title:"Slides.js", idea: "A platform that allows people to present their HTML5 slides and control them with their phones, tablets or even voice command, it also allows drawing on the slides and will have PDF support soon", builtWith: [{img:"img/Proj-logos/HTML5_LOGO.svg", href:""},{img:"img/Proj-logos/jQuery-Logo.png", href:"http://http://socket.io"},{img:"img/Proj-logos/nodejs-logo.png", href:""}], gitHub: false
+var slides = { pics: "img/proj-prints/slides.png", title:"Slides.js", idea: "A platform that allows people to present HTML5 slideshows and control them with their phones, tablets or even voice commands. It also allows drawing on the slides and will soon support PDF.", builtWith: [{img:"img/Proj-logos/HTML5_LOGO.svg", href:""},{img:"img/Proj-logos/jQuery-Logo.png", href:"http://http://socket.io"},{img:"img/Proj-logos/nodejs-logo.png", href:""}], gitHub: false
 };
 
 function fillProject(json){
@@ -40,6 +40,20 @@ function fillProject(json){
 	
 	jQuery.each(json.builtWith, function(i, src){
 		$("div.project div.body div.built-with").append("<div><img src='"+src.img+"' href='"+src.href+"'></div>");
+	});
+}
+
+function fillProjectTab(json){
+	//$("div.project div.photos").css("background-image", "url("+json.pics+")");
+	$("div.project-tab div.photo").empty();
+	$("div.project-tab div.photo").append("<img src='"+json.pics+"'>");
+	//$("div.project div.photos").text("foi");
+	$("div.project-tab div.column h1.title").text(json.title);
+	$("div.project-tab div.column p.description").text(json.idea);
+	$("div.project-tab div.column div.built-with").empty();
+	
+	jQuery.each(json.builtWith, function(i, src){
+		$("div.project-tab div.column div.built-with").append("<div><img src='"+src.img+"' href='"+src.href+"'></div>");
 	});
 }
 
@@ -81,7 +95,7 @@ $(function(){
 		var pic = $(this).css("background-image");
 		$("div.img-big").css("background-image", pic);
 	});
-	
+	/*
 	$("div.section-projects div.projects-list div.project-item.chat").click(function(){
 		fillProject(chat);
 		$(this).addClass("expanded");
@@ -107,6 +121,28 @@ $(function(){
 		$("div.whole").addClass("blur");
 		$("div.project").addClass("expanded");
 		
+	});
+	*/
+	
+	$("div.section-projects div.projects-list div.project-item.chat").click(function(){
+		fillProjectTab(chat);
+		$("div.project-tab").addClass("with-height");
+		$("div.project-tab div.triangle").removeClass("pos-1 pos-2 pos-3");
+		$("div.project-tab div.triangle").addClass("pos-1");
+	});
+	
+	$("div.section-projects div.projects-list div.project-item.tsk").click(function(){
+		fillProjectTab(tsk);
+		$("div.project-tab").addClass("with-height");
+		$("div.project-tab div.triangle").removeClass("pos-1 pos-2 pos-3");
+		$("div.project-tab div.triangle").addClass("pos-2");
+	});
+	
+	$("div.section-projects div.projects-list div.project-item.slides").click(function(){
+		fillProjectTab(slides);
+		$("div.project-tab").addClass("with-height");
+		$("div.project-tab div.triangle").removeClass("pos-1 pos-2 pos-3");
+		$("div.project-tab div.triangle").addClass("pos-3");
 	});
 	
 	$("div.cover").click(function(){
